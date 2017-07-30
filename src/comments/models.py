1b4f8be2d38f7d237eb2c83b9f0572561c9d7983
Vlad Melnichenko <vlad.melnuchenko96@gmail.com>
2017-07-30 23:21:25 +0300

    advancing part 12

--- src/comments/models.py
+++ src/comments/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from posts.models import Post
@@ -8,7 +10,12 @@ from posts.models import Post
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    post = models.ForeignKey(Post)
+    # post = models.ForeignKey(Post)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
