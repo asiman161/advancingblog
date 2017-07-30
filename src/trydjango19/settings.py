d45990eda46bc2c21afcafab68755bb65756f786
Vlad Melnichenko <vlad.melnuchenko96@gmail.com>
2017-07-30 19:11:25 +0300

    advancing part 9

--- src/trydjango19/settings.py
+++ src/trydjango19/settings.py
@@ -39,12 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # third party
+    'crispy_forms',
     'markdown_deux',
     'pagedown',
 
     #local
     'posts',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
