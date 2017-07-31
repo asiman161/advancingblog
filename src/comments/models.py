6e8019aee4f3b52f5bd23b1495a0fa5a8181c3bb
Vlad Melnichenko <vlad.melnuchenko96@gmail.com>
2017-07-31 14:49:41 +0300

    advancing part 16

--- src/comments/models.py
+++ src/comments/models.py
@@ -7,10 +7,13 @@ from django.db import models
 
 
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+
     def filter_by_instance(self, instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id
-        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id)
+        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id, parent=None)
         return qs
 
 
@@ -19,6 +22,7 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
     object_id = models.PositiveIntegerField(null=True)
     content_object = GenericForeignKey('content_type', 'object_id')
+    parent = models.ForeignKey('self', null=True, blank=True)
 
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -33,3 +37,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+    def children(self):  # replies
+        return Comment.objects.filter(parent=self)
+
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True
