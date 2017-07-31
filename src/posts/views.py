969ce5954d87e0b8cdac4091f220cda2e7cb7f14
Vlad Melnichenko <vlad.melnuchenko96@gmail.com>
2017-07-31 22:56:49 +0300

    advancing part 20

--- src/posts/views.py
+++ src/posts/views.py
@@ -10,7 +10,6 @@ from comments.forms import CommentForm
 from comments.models import Comment
 from .forms import PostForm
 from .models import Post
-from .utils import get_read_time
 
 
 def post_create(request):
@@ -35,8 +34,6 @@ def post_detail(request, slug=None):
     if instance.draft or instance.publish > timezone.now().date():
         if not request.user.is_staff or not request.user.is_superuser:
             raise Http404
-
-    print(get_read_time(instance.get_markdown()))
 
     initial_data = {
         'content_type': instance.get_content_type,
