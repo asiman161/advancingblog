9698de8d70ae6bec280ad6ace6c7e5a1e93eb0dd
Vlad Melnichenko <vlad.melnuchenko96@gmail.com>
2017-08-01 00:58:10 +0300

    advancing part 25

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -27,3 +27,40 @@ class UserLoginForm(forms.Form):
             if not user.is_active:
                 raise forms.ValidationError('This user is not longer active.')
         return super(UserLoginForm, self).clean(*args, **kwargs)
+
+
+class UserRegisterForm(forms.ModelForm):
+    email = forms.EmailField(label='Email address')
+    email2 = forms.EmailField(label='Confirm email')
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'email2',
+            'password',
+        ]
+
+    # def clean(self, *args, **kwards):
+    #     email = self.cleaned_data.get('email')
+    #     email2 = self.cleaned_data.get('email2')
+    #     if email != email2:
+    #         raise forms.ValidationError('Emails must match')
+    #
+    #     email_qs = User.objects.filter(email=email)
+    #     if email_qs.exists():
+    #         raise forms.ValidationError('This email has already registered')
+    #     return super(UserRegisterForm, self).clean(*args, **kwards)
+
+    def clean_email2(self):
+        email = self.cleaned_data.get('email')
+        email2 = self.cleaned_data.get('email2')
+        if email != email2:
+            raise forms.ValidationError('Emails must match')
+
+        email_qs = User.objects.filter(email=email)
+        if email_qs.exists():
+            raise forms.ValidationError('This email has already registered')
+        return email
